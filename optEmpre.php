aef0ca49498f6f08c9fed3e5009c46ef4ab5d803
hackuna885 <hackuna885@gmail.com>
2019-03-24 22:32:28 -0600

    Estamos a un 50%

---
+++ optEmpre.php
@@ -0,0 +1,99 @@
+<?php 
+include("assets/inc/iniPagina.inc");
+include("assets/inc/nav.inc"); 
+?>
+
+<!-- CONTENIDO -->
+<br>
+<br>
+<div class="container py-5">
+	<div class="row">
+		<div class="col-md-6">
+			<div class="container h-100">
+				<div class="d-flex h-100 align-items-center wow fadeInDown">
+					<table class="mx-auto">
+						<tr><td><h2 class="textoVerde py-4">Optimización Empresarial</h2></td></tr>
+						<tr><td><h4 class="text-muted py-2">+ INCREMENTAR VENTAS</h4></td></tr>
+						<tr><td><p class="text-justify">Potencializamos la capacidad económica de nuestros clientes a través de diversas estrategias comerciales, gestionando.</p></td></tr>
+					</table>
+				</div>
+			</div>
+		</div>
+		<div class="col-md-6 fondoImgEmpre wow fadeInUp">
+			<img class="img-fluid" src="img/circuloEmpresa.png" alt="Optimización Empresarial">
+		</div>
+	</div>
+</div>
+
+<div class="container">
+	<div class="row">
+		<div class="col-md-12">
+			<div class="container h-100">
+				<div class="d-flex h-100 align-items-center wow fadeInDown">
+					<table class="mx-auto">
+						<tr><td><h2 class="textoVerde py-4">SISTEMAS DE GESTIÓN</h2></td></tr>
+						<!-- <tr><td><h4 class="text-muted py-2">+ INCREMENTAR VENTAS</h4></td></tr>
+ -->						<tr><td><p class="text-justify">Implementamos un Sistema de Gestión Integral, con el objetivo de mejorar la eficiencia operativa que le permita a nuestros clientes medir, administrar y mejorar la calidad de sus productos y servicios enfocando los esfuerzos de la empresa hacia la rentabilidad y satisfacción del cliente dependiendo de su área de especialidad:</p></td></tr>
+					</table>
+				</div>
+			</div>
+		</div>
+		<div class="col-md-12 fondoImgEmpre wow fadeInUp py-5">
+			<div class="container-fluid">
+				<div class="row">
+					<div class="col-6 col-lg-2 text-center">
+						<h4 class="textoVerde d-flex justify-content-center align-items-center" style="height: 60px;">Calidad</h4>
+						<div class="cajaImg d-flex justify-content-center align-items-center">
+						<img class="img-fluid imgISOD" src="img/iso/9001_2015.svg" alt="Logo ISO 9001:2015 Calidad">
+						</div>
+					</div>
+					<div class="col-6 col-lg-2 text-center">
+						<h4 class="textoVerde d-flex justify-content-center align-items-center" style="height: 60px;">Seguridad Alimentaria</h4>
+						<div class="cajaImg d-flex justify-content-center align-items-center">
+						<img class="img-fluid imgISOD" src="img/iso/22000.svg" alt="Logo ISO 22000 Seguridad Alimentari">
+						</div>
+					</div>
+					<div class="col-6 col-lg-2 text-center">
+						<h4 class="textoVerde d-flex justify-content-center align-items-center" style="height: 60px;">Medio Ambiente</h4>
+						<div class="cajaImg d-flex justify-content-center align-items-center">
+						<img class="img-fluid imgISO" src="img/iso/14001_2015.svg" alt="Logo ISO 14001:2015 Medio Ambiente">
+						</div>
+					</div>
+					<div class="col-6 col-lg-2 text-center">
+						<h4 class="textoVerde d-flex justify-content-center align-items-center" style="height: 60px;">Energía</h4>
+						<div class="cajaImg d-flex justify-content-center align-items-center">
+						<img class="img-fluid imgISOD" src="img/iso/50001.svg" alt="Logo ISO 50001 Energía">
+						</div>
+					</div>
+					<div class="col-6 col-lg-2 text-center">
+						<h4 class="textoVerde d-flex justify-content-center align-items-center" style="height: 60px;">Riesgos</h4>
+						<div class="cajaImg d-flex justify-content-center align-items-center">
+						<img class="img-fluid imgISO" src="img/iso/31000.svg" alt="Logo ISO 31000 Riesgos">
+						</div>
+					</div>
+					<div class="col-6 col-lg-2 text-center">
+						<h4 class="textoVerde d-flex justify-content-center align-items-center" style="height: 60px;">Seguridad Laboral</h4>
+						<div class="cajaImg d-flex justify-content-center align-items-center">
+						<img class="img-fluid imgISO" src="img/iso/45001_2016.svg" alt="Logo ISO 45001:2016 Seguridad Laboral">
+						</div>
+					</div>
+				</div>
+			</div>
+
+		</div>
+		
+		
+	</div>
+</div>
+
+
+<br>
+<br>
+
+<!-- CONTENIDO -->
+
+
+<?php 
+include("assets/inc/footer.inc");
+include("assets/inc/finPagina.inc");
+?>
